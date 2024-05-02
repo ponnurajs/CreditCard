ab11f993b8b328830c72b329ee762f4edcafd287
ponnuraj <ponnurajs@gmail.com>
2024-05-01 20:06:51 -0600

    added table

--- CreditCardAdmin/src/app/header/header.component.ts
+++ CreditCardAdmin/src/app/header/header.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
+import { MatIcon, MatIconModule } from '@angular/material/icon';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import { AddComponent } from '../add/add.component';
 import { RouterOutlet } from '@angular/router';
@@ -9,47 +9,48 @@ import { DeleteComponent } from '../delete/delete.component';
 import { SidemenuComponent } from '../sidemenu/sidemenu.component';
 import {MatCardModule} from '@angular/material/card';
 import { Creditcard } from '../model/creditcard';
-import {MatTable, MatTableDataSource, MatTableModule} from '@angular/material/table';
+import {MatTableModule} from '@angular/material/table';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 
 const TABLE_DATA : Creditcard[] = [
   {  id:1,
-    name:"Bank of America",
-    maxCredit:1000,
-    interestRate: 2.3,
-    active:true,
-    recommendedScore: "700-900"
-  },
-  {  id:2,
-    name:"Bank of America",
-    maxCredit:1000,
-    interestRate: 2.3,
-    active:true,
-    recommendedScore: "700-900"
-  },
-  {  id:3,
-    name:"Bank of America",
-    maxCredit:1000,
-    interestRate: 2.3,
-    active:true,
-    recommendedScore: "700-900"
-  }
-];
+      name:"Bank of America",
+      maxCredit:1000,
+      interestRate: 2.3,
+      active:true,
+      recommendedScore: "700-900"
+    },
+    {  id:2,
+      name:"Bank of America",
+      maxCredit:1000,
+      interestRate: 2.3,
+      active:true,
+      recommendedScore: "700-900"
+    },
+    {  id:3,
+      name:"Bank of America",
+      maxCredit:1000,
+      interestRate: 2.3,
+      active:true,
+      recommendedScore: "700-900"
+    }
+  ];
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [MatToolbarModule, MatIconModule, MatSidenavModule, 
     SidemenuComponent, AddComponent, ViewComponent, DeleteComponent, 
-    RouterOutlet, MatCardModule],
+    RouterOutlet, MatCardModule, MatTableModule, MatCheckboxModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
   sideToggle:boolean = false;
 
-  dispalyColumn = ['select', 'id', 'name', 'maxCredit', 'InterestRate', 'RecommededScore', 'Active'];
+  dispalyColumn = ['id', 'name', 'maxCredit', 'InterestRate', 'RecommendedScore', 'Active', 'Actions'];
 
-  dataSource = new MatTableDataSource(TABLE_DATA);
+  dataSource = TABLE_DATA;
 
   togglesidebar(){
     this.sideToggle = this.sideToggle? false : true;
